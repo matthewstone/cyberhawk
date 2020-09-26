c59fb72a35d502f4f1ecb253e421afbfa90419a0
Matt Stone <stone.matthew@gmail.com>
2020-09-26 03:10:51 +0100

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,51 @@
+<?php
+use DI\Container;
+use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+
+$container = new Container();
+AppFactory::setContainer($container);
+
+$container->set('view', function() {
+    return Twig::create(__DIR__ . '/../templates',
+        ['cache' => __DIR__ . '/../cache']);
+});
+
+$app = AppFactory::create();
+
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::createFromContainer($app));
+
+$app->get('/', function ($request, $response, $args) {
+    $turbines = range(0, 99);
+    foreach ($turbines as $k => $turbine) {
+        $turbines[$k] = [];
+        $turbines[$k]['id'] = $k;
+        $turbines[$k]['errors'] = [];
+        if($k === 0) { //division by 0
+            continue;
+        }
+        if($k % 3 == 0) {
+            //echo "Has error!";
+            $turbines[$k]['errors'][] = ['message' => 'coating damage'];
+        }
+        if($k %5 == 0) {
+            $turbines[$k]['errors'][] = ['message' => 'lightning strike'];
+        }
+    }
+    return $this->get('view')->render($response, 'index.twig', [
+        'turbines' => $turbines
+    ]);
+    //foreach($turbines as $turbine) {
+    //    $out = print_r($turbine, true);
+    //    $response->getBody()->write($out);
+    //}
+    //$response->getBody()->write(print_r($turbines, true));
+    //return $response;
+});
+
+$app->run();
